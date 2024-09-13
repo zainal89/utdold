79318b4094f8361fe1c1845661bd326f71c04601
epromroffca10 <83897057+epromroffca10@users.noreply.github.com>
2024-09-13 23:12:50 +0800

    db set

--- config/func_koneksi.php
+++ config/func_koneksi.php
@@ -10,7 +10,7 @@ define("db_pwd", "");
 define("db_dbase", "db_utdold");
 
 // define("db_user", "takt3736_osp"); 
-// define("db_pwd", "Absen???21");
+// define("db_pwd", "Absen???21");//
 // define("db_dbase", "takt3736_osp");
 
 $mysqli = new mysqli(db_host, db_user, db_pwd, db_dbase);
